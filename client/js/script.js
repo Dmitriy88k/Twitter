857c9fd96a8696f7b200a9aa47ea6f40ae8033e5
Almat Ybray <a.ybray@sdventures.com>
2023-11-05 20:50:31 +0300

    refactor login form

--- client/js/script.js
+++ client/js/script.js
@@ -1,51 +1,43 @@
-function clearPlaceholder(input) {
-  input.placeholder = "";
-}
-
-function restorePlaceholder(input, defaultPlaceholder) {
-  if (input.value === '') {
-    input.placeholder = defaultPlaceholder;
-  }
-}
-
 const loginEmail = document.getElementById("login-email");
 const loginPassword = document.getElementById("login-password");
-const loginButton = document.getElementById("login-button");
+const form = document.getElementById("form");
 
+function validateEmail(loginEmailInput) {
+  const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(loginEmailInput);
+}
 
-loginButton.addEventListener("click", function() {
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
   const loginEmailInput = loginEmail.value;
   const loginPasswordInput = loginPassword.value;
   const loginUrl = "http://167.71.82.123:8081/signup";
+  let error = {}
 
-  fetch(loginUrl).then(response => response.json()).then((data) => {
-    function validateEmail(loginEmailInput) {
-      const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-      return regex.test(loginEmailInput);
-    }
-    
-    if (validateEmail(loginEmailInput)) {
-      console.log("Valid email address.");
-    } else {
-      console.log("Invalid email address.");
-    }
-    if (loginEmailInput === data.email && loginPasswordInput === data.password) {
-      console.log("everything is good");
-    }
+  if (loginEmailInput === "" || loginEmailInput.trim() === '') {
+    error.email = 'Email field is empty'
+  } else if (!validateEmail(loginEmailInput)) {
+    error.email = 'Email is incorrect'
+  }
+
+  if (loginPasswordInput === "" || loginPasswordInput.trim() === '') {
+    error.password = 'Password field is empty'
+  }
+  
+  if (Object.keys(error).length > 0) {
+    alert(JSON.stringify(error, null, 4))
+    console.log(JSON.stringify(error, null, 4));
+
+    return
+  }
+
+  fetch(loginUrl, {
+    method: 'POST',
+    credentials: 'include',
+  }).then(response => response.text()).then((data) => {
+    console.log(data);
   }).catch(error => {
-    if (loginEmailInput === "" && loginPasswordInput === "") {
-      alert("Email and password are empty");
-    }
-    if (loginEmailInput === "") {
-      alert("Email field is empty");
-    }
-    if (loginPasswordInput === "") {
-      alert("Password field is empty");
-    }
+    console.log('error', error)
   });
-});
-
-
-
-
-
+})
